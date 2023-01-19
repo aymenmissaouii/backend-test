7b57330ff8d12a7858d812723bbdb9a3c5edec19
aymen missaoui <aymenmissaoui56@gmail.com>
2023-01-19 22:20:41 +0100

    Update ExchangeRateService.php

--- src/Service/ExchangeRateService.php
+++ src/Service/ExchangeRateService.php
@@ -12,6 +12,12 @@ class ExchangeRateService {
      * @return array<string>
      */
     public function getSupportedCurrencies(): array {
+
+        $response = $this->getAmountApi(0,'','',"symbols");
+        
+        $decoderesopnse = json_decode($response, true);
+
+        return array_keys($decoderesopnse['symbols']);
     }
 
     /**
@@ -23,5 +29,36 @@ class ExchangeRateService {
      * @return int
      */
     public function getExchangeAmount(int $amount, string $fromCurrency, string $toCurrency): int {
+
+        $response = $this->getAmountApi($amount,$fromCurrency,$toCurrency,"convert?to={$toCurrency}&from={$fromCurrency}&amount={$amount}");
+   
+        $decoderesopnse = json_decode($response, true);
+       
+        return $decoderesopnse['result'];
+    }
+
+
+    public function getAmountApi(int $amount, string $fromCurrency, string $toCurrency, string $api): string{
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.apilayer.com/exchangerates_data/{$api}",
+          CURLOPT_HTTPHEADER => array(
+            "Content-Type: text/plain",
+            "apikey: l8oDcGxFWRsxX0W4GZGt7vbLiYLJUAHm"
+          ),
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET"
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        return $response;
     }
 }
